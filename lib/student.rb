14254f1920b1828f964984466843f030f89017b2
Ben <henriquez.ben@gmail.com>
2016-10-08 22:23:33 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,29 +1,54 @@
 class Student
   attr_accessor :id, :name, :grade
 
+  @@all = []
+
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+  new_student = self.new
+  new_student.id = row[0]
+  new_student.name =  row[1]
+  new_student.grade = row[2]
+
+  new_student
+
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+   sql = <<-SQL
+     SELECT *
+     FROM students
+   SQL
+
+   DB[:conn].execute(sql)
+    student = self.new_from_db(row)
+    @@all << student
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,name).map do |row|
+        self.new_from_db(row)
+
+    end.first
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
@@ -40,4 +65,40 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.count_all_students_in_grade_9
+    sql = "select count(grade = 9) from students"
+    DB[:conn].execute(sql)
+  end
+
+  def self.students_below_12th_grade
+    sql = "select name from students group by name having grade < 12"
+    DB[:conn].execute(sql)
+    array = []
+    array << sql
+    array
+  end
+
+  def self.all
+    sql = "select * from students"
+    DB[:conn].execute(sql).map {|row| self.new_from_db(row)}
+  end
+
+  def self.first_x_students_in_grade_10(num)
+    sql = "select name from students group by id having grade = 10 limit ?;"
+    DB[:conn].execute(sql, num)
+  end
+
+  def self.first_student_in_grade_10
+    sql = "select * from students where grade = 10 order by students.id asc limit 1;"
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.all_students_in_grade_x(num)
+    sql = "select * from students group by name having grade = ?"
+    DB[:conn].execute(sql, num)
+  end
+
 end
